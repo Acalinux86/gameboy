1f680d183e248fde11d251e2aed6399e402e4507
Ahmed Ali <ahmedcali1984@gmail.com>
2026-01-10 11:04:28 +0300

    [gb.c] Remove code

--- src/gb.c
+++ src/gb.c
@@ -7,18 +7,14 @@ int main(void)
     const char *log_file_path = "gameboy.log";
     gb_log_init(gb_dir, log_file_path, GB_LOG_INFO);
 
-    for (int i = 0; i < Logger.files->count; ++i) {
-        GB_INFO("%s", gb__log_get_src_file(Logger.files, i));
-    }
-
-    GbMemoryMap mmu = {0};
+    static GbMemoryMap mmu = {0};
     GB_INFO("Initializing Memory");
 
     gb_mmu_init(&mmu);
     GB_INFO("Memory initialized");
 
     const int location = 0xA100;
-    const int count = 1;
+    const int count = 500;
     for (int i = 0; i < count; ++i) {
         const int data = i + 1;
         if (!gb_mmu_write(&mmu, location + i, data)) {
@@ -27,12 +23,6 @@ int main(void)
         }
         GB_INFO("Successfully wrote => %d to %d", data, location + i);
     }
-
-    for (int i = 0; i < count; ++i) {
-        const int data = gb_mmu_read(&mmu, location + i);
-        GB_INFO("Successfully Read => %d from %d", data, location + i);
-    }
-
     gb_mmu_destroy(&mmu);
     GB_INFO("Memory Destroyed");
     gb_log_shutdown();
