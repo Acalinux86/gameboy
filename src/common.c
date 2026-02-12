b0b2c023187c78ee131cce1396da2c5bb4be5169
Ahmed Ali <ahmedcali1984@gmail.com>
2026-02-12 17:07:10 +0300

    [common.c] Minor Changes

--- src/common.c
+++ src/common.c
@@ -7,20 +7,20 @@ unsigned char *read_file(const char *filepath, size_t *file_size)
 {
     FILE *fp = fopen(filepath, "rb");
     if (fp == NULL) {
-        fprintf(stderr, "[ERROR] Could not read `%s`: `%s`\n", filepath, strerror(errno));
+        fprintf(stderr, "ERROR: Could not read `%s`: `%s`\n", filepath, strerror(errno));
         return NULL;
     }
 
     int ret = fseek(fp, 0, SEEK_END);
     if (ret < 0) {
-        fprintf(stderr, "[ERROR] Could not seek to end of `%s`: `%s`\n", filepath, strerror(errno));
+        fprintf(stderr, "ERROR: Could not seek to end of `%s`: `%s`\n", filepath, strerror(errno));
         fclose(fp);
         return NULL;
     }
 
     size_t size = ftell(fp);
     if (size <= 0) {
-        fprintf(stderr, "[ERROR] File `%s` Empty\n", filepath);
+        fprintf(stderr, "ERROR: File `%s` Empty\n", filepath);
         fclose(fp);
         return NULL;
     }
@@ -29,14 +29,14 @@ unsigned char *read_file(const char *filepath, size_t *file_size)
 
     unsigned char *buffer = (unsigned char *)malloc(size + 1);
     if (buffer == NULL) {
-        fprintf(stderr, "[ERROR] Memory Allocation Failed\n");
+        fprintf(stderr, "ERROR: Memory Allocation Failed\n");
         fclose(fp);
         return NULL;
     }
 
     size_t bytes = fread(buffer, sizeof(*buffer), size, fp);
     if (bytes != (size_t)size) {
-        fprintf(stderr, "fread() failed: Expected %ld bytes got %zu bytes\n", size, bytes);
+        fprintf(stderr, "ERROR: fread() failed: Expected %ld bytes got %zu bytes\n", size, bytes);
         free(buffer);
         fclose(fp);
         return NULL;
