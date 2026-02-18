ceb0b82f00cb683c65f4f4542900d1e2f8c6137c
Ahmed Ali <ahmedcali1984@gmail.com>
2026-02-18 22:23:16 +0300

    [gb.c] Update Main File

--- src/gb.c
+++ src/gb.c
@@ -6,36 +6,23 @@
 
 int main(void)
 {
+    /* Open the Rom File into memory */
+    const char *file_path = "data/roms/tetris.gb";
+
+    FILE *gfp = fopen(file_path, "rb");
+    if (gfp == NULL)
+    {
+        fprintf(stderr, "ERROR: Failed To Open file: `%s`\n", file_path);
+        return -1;
+    }
+
     /* Initialize the Cpu */
     struct SM83CPU cpu = {0};
-    int ret = sm83_cpu_init(&cpu, INITIALIZE_PROGRAM_COUNTER);
+    int ret = sm83_cpu_init(&cpu, gfp, INITIALIZE_PROGRAM_COUNTER);
     if (ret != 0) return 1;
 
     /* Emit Disassembly */
     sm83_cpu_emit_disasm(&cpu, 1);
-
-    /* Read the Rom File into memory */
-    size_t size = 0;
-    const char *file_path = "data/roms/test.gb";
-    unsigned char *buffer = read_file(file_path, &size);
-
-    /* Write the Rom file into the Gameboy ROM */
-    for (int i = 0; i < (int)size; ++i)
-    {
-        if (gb_mmu_write(cpu.mmu, cpu.registers.pc + i, buffer[i]) != 0)
-        {
-            /* Free Buffer if write fails */
-            fprintf(stderr, "ERROR: Write Failed\n");
-            free(buffer);
-            return 1;
-        }
-    }
-
-    /* 
-        After Loading the Rom to the MMU->ROM, 
-        disable write Access to the ROM 
-    */
-    gb_mmu_disable_write_access_to_rom(cpu.mmu);
 
     /* Fetch => Decode => Execute Loop Cycle */
     while (1)
@@ -62,11 +49,11 @@ int main(void)
     /* Dump the Disassembly */
     sm83_dump_disasm(&cpu.disasm, fp);
 
-    /* Close the File Pointer */
+    /* Close Disassembly File Pointer */
     fclose(fp);
 
-    /* Free the Allocated Rom Buffer */
-    free(buffer);
+    /* Close Game File Pointer */
+    fclose(gfp);
 
     /* Shutdown the CPU */
     sm83_cpu_shutdown(&cpu);
